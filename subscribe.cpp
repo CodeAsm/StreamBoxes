78ff4e607db9de5aad06293ca5e1c67cfa85217d
Nico Vijlbrief <nico.vijlbrief@outlook.com>
2024-08-27 12:10:26 +0200

    Added a few more boxes

---
+++ subscribe.cpp
@@ -0,0 +1,14 @@
+#include <final/final.h>
+
+auto main (int argc, char* argv[]) -> int
+{
+  finalcut::FApplication app(argc, argv);
+  finalcut::FDialog dialog(&app);
+  dialog.setText ("Subscribe");
+  const finalcut::FPoint position{25, 5};
+  const finalcut::FSize size{30, 10};
+  dialog.setGeometry (position, size);
+  finalcut::FWidget::setMainWidget(&dialog);
+  dialog.show();
+  return app.exec();
+}
